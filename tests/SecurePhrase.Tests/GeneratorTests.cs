c183ca8b1dccc2c09ee7b50faaf4cf1be11d4f7e
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-05-31 18:31:45 +0300

    Add unit tests for passphrase generation functionality

---
+++ tests/SecurePhrase.Tests/GeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace SecurePhrase.Tests;
+
+public class GeneratorTests
+{
+    [Fact]
+    public void DefaultPassphraseLength_ShouldBeFive()
+    {
+        var passphrase = Passphrase.Generate();
+
+        var passphraseLength = passphrase.Split("-").Length;
+
+        Assert.NotNull(passphrase);
+        Assert.Equal(5, passphraseLength);
+    }
+
+    [Fact]
+    public void Should_return_passphrase_of_length_10()
+    {
+        var passphrase = Passphrase.Generate(10);
+
+        var passphraseLength = passphrase.Split("-").Length;
+
+        Assert.NotNull(passphrase);
+        Assert.Equal(10, passphraseLength);
+    }
+}
